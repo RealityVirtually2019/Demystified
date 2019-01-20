fdf243198637ce1a168377e3dcedac81d1e5f618
Ciera <Ciera.jones.cj@gmail.com>
2019-01-19 23:42:41 -0500

    last commit of evening

---
+++ demystified-magicleap/Assets/_Ciera/Scripts/DisableChildren.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR 
+using UnityEditor;
+[CustomEditor(typeof(DisableChildren))]
+public class DisableChildrenEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		DisableChildren myScript = (DisableChildren)target;
+
+		if (GUILayout.Button("Disable Children"))
+		{
+			myScript.DisableAllChildren ();
+		}
+
+	}
+}
+#endif 
+public class DisableChildren : MonoBehaviour
+{
+	public void DisableChildrenExceptFor (Transform EnableThisChild)
+	{
+		if (EnableThisChild != null) { 
+			for (int i = 0; i < transform.childCount; i++) {
+
+				var currentChild = transform.GetChild (i);
+
+				if (currentChild == EnableThisChild) {
+					transform.GetChild (i).gameObject.SetActive (true);
+				} else {
+					transform.GetChild (i).gameObject.SetActive (false);
+				}
+			}
+		} 
+	}
+
+	public void DisableAllChildren ()
+	{
+		for (int i = 0; i < transform.childCount; i++) {
+
+			transform.GetChild (i).gameObject.SetActive (false);
+		}
+	}
+    public void EnableAllChildren()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+
+            transform.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+}
