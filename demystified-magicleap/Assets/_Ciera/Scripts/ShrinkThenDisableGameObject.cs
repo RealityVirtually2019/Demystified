43a1bf7e37bbc79aab1ea81965b319a1b7749efb
Ciera <Ciera.jones.cj@gmail.com>
2019-01-19 21:31:16 -0500

    its fine

---
+++ demystified-magicleap/Assets/_Ciera/Scripts/ShrinkThenDisableGameObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkThenDisableGameObject : MonoBehaviour {
+
+	Vector3 StartSizeOfObjectToGrow;
+	public float TimeOfChange = 1; 
+	// Use this for initialization
+	void OnEnable () {
+	
+		StartCoroutine (ShrinkingDown ());
+	}
+
+	IEnumerator ShrinkingDown ()
+	{
+
+		StartSizeOfObjectToGrow = transform.localScale;
+
+		float progress = 0;
+		while (progress < 1)
+		{
+			transform.localScale = Vector3.Slerp (transform.localScale, Vector3.zero, progress);
+
+			progress += Time.deltaTime / TimeOfChange;
+			yield return null;
+		}
+
+		gameObject.SetActive (false);
+		transform.localScale = StartSizeOfObjectToGrow;
+
+		yield return null;
+
+	}
+}
