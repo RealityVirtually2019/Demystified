d62006aee8400353f64ace21a5c5125f32afe1ff
Ciera <Ciera.jones.cj@gmail.com>
2019-01-20 09:53:12 -0500

    moar audya

---
+++ demystified-magicleap/Assets/_Ciera/Scripts/ChangeMaterialColor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterialColor : MonoBehaviour {
+
+    public Material materialToChange;
+    public string PropertyToChange = "_TintColor";
+    public Gradient colorGradient;
+    float colorValue;
+    public float time;
+    // Use this for initialization
+    void Awake () {
+
+        materialToChange.EnableKeyword(PropertyToChange);
+
+    }
+    public void StartChangeColor(){
+        materialToChange.EnableKeyword(PropertyToChange);
+        StartCoroutine(ChangeColor());
+    }
+	private void OnEnable()
+	{
+        materialToChange.SetColor(PropertyToChange,colorGradient.Evaluate(0));
+		
+	}
+	private void OnApplicationQuit()
+	{
+        materialToChange.SetColor(PropertyToChange,colorGradient.Evaluate(0));
+	}
+    private void OnDisable(){
+        materialToChange.SetColor(PropertyToChange,colorGradient.Evaluate(0));
+
+    }
+    IEnumerator ChangeColor()
+    {
+        float elapsedTime = 0;
+        while (elapsedTime < (time))
+        {
+            Debug.Log("fading");
+            elapsedTime += Time.deltaTime;
+            materialToChange.SetColor(PropertyToChange, colorGradient.Evaluate(elapsedTime / time));
+
+          //  SpriteToChange.color = Color.Lerp(SpriteToChange.color, ColorToChangeTo, elapsedTime /time);
+            yield return null;
+        }
+        yield return null;
+
+    }
+	//utilites lerp color from gradient, need : min, max, gradient, colortoChange
+}
