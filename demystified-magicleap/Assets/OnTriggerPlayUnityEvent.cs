c6ca505a4baad7ac5fea157fa41d8a6b41426c04
Ciera <Ciera.jones.cj@gmail.com>
2019-01-20 12:51:17 -0500

    experiemental feature didnt work!

---
+++ demystified-magicleap/Assets/OnTriggerPlayUnityEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+using UnityEngine.XR.MagicLeap;
+[RequireComponent(typeof(ControllerConnectionHandler))]
+public class OnTriggerPlayUnityEvent : MonoBehaviour
+{
+
+    public UnityEvent OnTrigger;
+    private ControllerConnectionHandler _controllerConnectionHandler;
+
+
+    private void Start()
+    {
+        _controllerConnectionHandler = GetComponent<ControllerConnectionHandler>();
+        MLInput.OnTriggerDown += HandleOnTriggerDown;
+    }
+
+    private void HandleOnTriggerDown(byte controllerId, float value)
+    {
+        OnTrigger.Invoke();
+    }
+
+
+}
